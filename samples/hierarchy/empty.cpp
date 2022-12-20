51c3c05d8c2ceebc5d20b13cf9a587200ad53c33
Stuart Scott <stuart.murray.scott@gmail.com>
2022-12-20 00:34:04 -0800

    discussions/22

---
+++ samples/hierarchy/empty.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+#include <Wink/address.h>
+#include <Wink/machine.h>
+#include <Wink/state.h>
+
+int main(int argc, char **argv) {
+  if (argc < 3) {
+    error() << "Incorrect parameters, expected <spawner> <address>\n"
+            << std::flush;
+    return -1;
+  }
+
+  Address spawner(argv[1]);
+  Address address(argv[2]);
+  UDPSocket socket;
+  Machine m(spawner, address, "hierarchy/Empty", socket);
+
+  m.Start();
+}
